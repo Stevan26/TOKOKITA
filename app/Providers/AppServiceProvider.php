2c16655d47d2b5b42f592cf2f244b078c2fb1178
stevan26 <stevanpanjaitan433@gmail.com>
2026-07-07 21:52:49 +0700

    fix: registrasi Cloudinary service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,23 +5,24 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use App\Models\User;
-use Illuminate\Http\UploadedFile; // WAJIB ADA
+use Illuminate\Http\UploadedFile;
+use CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider; // Import ini
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        //
+        // Daftarkan Cloudinary Service ke dalam Container Laravel
+        $this->app->register(CloudinaryServiceProvider::class);
     }
 
     public function boot(): void
     {
-        // 1. Aturan hak akses admin yang sudah kamu buat
         Gate::define('kelola-produk', function (User $user) {
             return $user->role === 'admin';
         });
 
-        // 2. Tambahkan Macro untuk Cloudinary agar tidak error lagi
+        // Tetap pertahankan macro ini
         if (!UploadedFile::hasMacro('storeOnCloudinary')) {
             UploadedFile::macro('storeOnCloudinary', function ($folder = null, $publicId = null) {
                 return app('cloudinary')->upload($this->getRealPath(), [
